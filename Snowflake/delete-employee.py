09dbd5e26cb6590a947266261e77428765d8a9fb
pradeepkudukkil <88960052+pradeepkudukkil@users.noreply.github.com>
2025-01-24 12:17:51 +0530

    Update delete-employee.py

--- Snowflake/delete-employee.py
+++ Snowflake/delete-employee.py
@@ -1,1 +1,26 @@
+import snowflake.connector
 
+# Connect to Snowflake
+connection = snowflake.connector.connect(
+    user='PRADEEPKUDUKKIL',
+    password='Test@7376@',
+    account='zsbuhwf-bp99278',  # e.g., xy12345.us-east-1
+    database='PRADEEP_TEST',
+    schema='MIGRATIONS',  # Optional
+    warehouse='COMPUTE_WH',  # Optional
+    role='ACCOUNTADMIN'  # Optional
+)
+
+# Create a cursor object
+cursor = connection.cursor()
+
+try:
+    delete_table_query = "DROP TABLE IF EXISTS employees;"
+    cursor.execute(delete_table_query)
+    print("The 'employee' table has been deleted successfully.")
+    
+
+finally:
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
