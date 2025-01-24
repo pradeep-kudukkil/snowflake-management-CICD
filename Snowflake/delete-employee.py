1ac0e8defbb7855990ac6a1aff829aac4b583210
pradeepkudukkil <88960052+pradeepkudukkil@users.noreply.github.com>
2025-01-24 18:09:22 +0530

    Update delete-employee.py

--- Snowflake/delete-employee.py
+++ Snowflake/delete-employee.py
@@ -2,10 +2,10 @@ import snowflake.connector
 
 # Connect to Snowflake
 connection = snowflake.connector.connect(
-    user='PRADEEPKUDUKKIL',
-    password='m3fc9hK@2nURmdd',
+    user='username',
+    password='password',
     account='zsbuhwf-bp99278',  # e.g., xy12345.us-east-1
-    database='PRADEEP_TEST',
+    database='db_name',
     schema='MIGRATIONS',  # Optional
     warehouse='COMPUTE_WH',  # Optional
     role='ACCOUNTADMIN'  # Optional
